87acab10d61f4811ddb78f0daf0faaa65f108ae9
Nyctezz <aigor.brzakala@gmail.com>
2026-06-30 22:54:24 +0200

    Empty window + major cleaning

--- src/main.c
+++ src/main.c
@@ -1,6 +1,44 @@
 #include <SDL3/SDL.h>
+#include <stdio.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        printf("SDL_Init failed: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow(
+        "ChemistryEngineeringGame",
+        800, 600,
+        0
+    );
+
+    if (!window)
+    {
+        printf("Window creation failed: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    int running = 1;
+    SDL_Event event;
+
+    while (running)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_EVENT_QUIT)
+            {
+                running = 0;
+            }
+        }
+
+        SDL_Delay(16); // ~60 FPS cap
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
